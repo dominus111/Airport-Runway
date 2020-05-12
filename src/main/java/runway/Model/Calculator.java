1587ae266a476e90f218fd793ba8e89418af5838
Martin Kanev <m.kanev99@gmail.com>
2020-05-12 20:08:24 +0300

    wrong branch

--- src/main/java/runway/Model/Calculator.java
+++ src/main/java/runway/Model/Calculator.java
@@ -8,6 +8,11 @@ public class Calculator {
     private final static int distToCenterL = 75;
     private final static int RESA = 240;
     private final static int BLAST_ALLOWENCE = 300;
+    boolean landing = false;
+
+    public void setLanding(boolean l){
+        this.landing = l;
+    }
 
     public void calculate(ObstaclePositionParam oParam, Runway runway) {
         // Checks if further calculations are needed
@@ -27,37 +32,46 @@ public class Calculator {
         double objHeight = oParam.getObstacle().getHeight();
 
         // Decide which runway is used for takes off/landing towards/away from the obstacle
-        if (oParam.getDistToLTHR() < oParam.getDistToRTHR()) {
-            takeOffAwayLandOver(objHeight, oParam.getDistToLTHR(), runway.getLeftRunway());
-            takeOffTowardsLandTowards(rDisplacement, objHeight, oParam.getDistToRTHR(), runway.getRightRunway());
+        if ( runway.getLeftRunway().getInitialParameters().getTora() / 2 > oParam.getDistToLTHR()) {
+            takeOffAwayLandOver(objHeight, oParam.getDistToLTHR(), runway.getLeftRunway(),landing);
         } else {
-            takeOffAwayLandOver(objHeight, oParam.getDistToRTHR(), runway.getRightRunway());
-            takeOffTowardsLandTowards(lDisplacemnet, objHeight, oParam.getDistToLTHR(), runway.getLeftRunway());
+            takeOffTowardsLandTowards(objHeight, oParam.getDistToLTHR(), runway.getLeftRunway(), landing);
         }
+        if ( runway.getRightRunway().getInitialParameters().getTora() / 2 > oParam.getDistToRTHR()) {
+            takeOffAwayLandOver(objHeight, oParam.getDistToRTHR(), runway.getRightRunway(), landing);
+        } else {
+            takeOffTowardsLandTowards(objHeight, oParam.getDistToRTHR(), runway.getRightRunway(), landing);
+        }
+
     }
 
-    public void  takeOffAwayLandOver(double height, double distToTHR, VirtualRunway runway) {
+
+
+    public void  takeOffAwayLandOver(double height, double distToTHR, VirtualRunway runway, boolean land) {
         // Initial necessary values
         RunwayParameters runwayParam = runway.getInitialParameters();
         double stopway = runwayParam.getAsda() - runwayParam.getTora();
         double clearway = runwayParam.getToda() - runwayParam.getTora();
-        double displacedTHR = runwayParam.getdispTHR();
+        //double displacedTHR = runwayParam.getdispTHR();
         String calculationBrkdwn = "";
 
-        double newTora, newLda;
-        if ((stripEnd + RESA) > BLAST_ALLOWENCE) {
-            newTora = runwayParam.getTora() - displacedTHR - distToTHR - (stripEnd + RESA);
-            calculationBrkdwn+="TORA = Initial TORA - Displaced Threshold - Distance from Threshold - Strip End - RESA\n" +
-                    "TORA = "+runwayParam.getTora()+" - "+displacedTHR+" - "+distToTHR+" - "+ (stripEnd + RESA)+"\n" +
-                    "TORA = "+newTora+"\n \n";
+        double newTora = 0, newLda = 0, newToda = 0, newAsda = 0;
+        if (land) {
+            newLda = runwayParam.getTora() - distToTHR - (stripEnd + RESA) - (50*height);
+            calculationBrkdwn+="LDA = Tora - Object treshold - Strip end - RESA - height*slope\n" +
+                    "LDA = "+runwayParam.getLda();
 
         } else {
-            newTora = runwayParam.getTora() - displacedTHR - distToTHR - BLAST_ALLOWENCE;
-            calculationBrkdwn+="TORA = Initial TORA - Displaced Threshold - Distance from Threshold  - Blast Protection\n" +
-                    "TORA = "+runwayParam.getTora()+" - "+displacedTHR+" - "+distToTHR+" - "+ BLAST_ALLOWENCE +"\n" +
-                    "TORA = "+newTora+ "\n \n";
+            newTora = runwayParam.getTora() - distToTHR - (stripEnd + RESA);
+            newAsda = runwayParam.getAsda() - distToTHR - (stripEnd + RESA);
+            newToda = runwayParam.getToda() - distToTHR - (stripEnd + RESA);
+            calculationBrkdwn+="TORA = Initial TORA - Object Threshold - Strip End - RESA\n" +
+                    "TORA = "+runwayParam.getTora()+" - "+distToTHR+" - "+ (stripEnd + RESA)+"\n" +
+                    "TORA = "+newTora+"\n \n" +
+            "ASDA = " + newAsda+ "\n \n" +
+            "TODA = "+newToda+ " \n \n";
         }
-
+        /*
         if(((50 * height + stripEnd) > BLAST_ALLOWENCE)) {
             newLda = runwayParam.getLda() - distToTHR - ((50 * height + stripEnd));
             calculationBrkdwn+="LDA = Initial LDA - Distance from Threshold - Slope Angle * Obstacle Height + Strip End\n"+
@@ -71,26 +85,42 @@ public class Calculator {
                     "LDA = "+newLda+ "\n \n";
         }
 
-        double newAsda = newTora + stopway;
-        double newToda = newTora + clearway;
-        calculationBrkdwn+="ASDA = TORA + stopway\n"+"ASDA = "+newTora+" + "+stopway+"\n" +"ASDA = "+newAsda+"\n \n";
-        calculationBrkdwn+="TODA = TORA + clearway\n"+"TODA = "+newTora+" + "+clearway+"\n"+ "TODA = "+newToda+"\n \n";
-
+        //calculationBrkdwn+="ASDA = TORA + stopway\n"+"ASDA = "+newTora+" + "+stopway+"\n" +"ASDA = "+newAsda+"\n \n";
+        //calculationBrkdwn+="TODA = TORA + clearway\n"+"TODA = "+newTora+" + "+clearway+"\n"+ "TODA = "+newToda+"\n \n";
+        */
         runway.setRecalculatedParameters(new RunwayParameters(newTora,newToda,newAsda,newLda, 0));
         runway.getRecalculatedParameters().setCalculationBrkdwn(calculationBrkdwn);
     }
 
 
-    public void  takeOffTowardsLandTowards(double displacedTHR, double height, double distToTHR, VirtualRunway runway) {
+    public void  takeOffTowardsLandTowards( double height, double distToTHR, VirtualRunway runway, boolean land) {
 
+        RunwayParameters runwayParam = runway.getInitialParameters();
         String calculationBrkdwn = "";
+        double distT = runwayParam.getTora()- distToTHR;
         double slopeValue = 50 * height;
         if (slopeValue < RESA)
             slopeValue = RESA;
-        double newTORA = distToTHR + displacedTHR - slopeValue - stripEnd;
-        double newLDA = distToTHR - RESA - stripEnd;
+        double newTora = 0, newLda = 0, newToda = 0, newAsda = 0;
+
+        if (land) {
+            newLda = runwayParam.getTora() - distT - (stripEnd + RESA);
+            calculationBrkdwn+="LDA = Tora - Object treshold - Strip end - RESA\n" +
+                    "LDA = "+runwayParam.getLda();
+
+        } else {
+            newTora = runwayParam.getTora() - distT - (stripEnd + RESA)- (50*height);
+            newAsda = runwayParam.getAsda() - distT - (stripEnd + RESA)- (50*height);
+            newToda = newTora;
+            calculationBrkdwn+="TORA = Initial TORA - Object Threshold - Strip End - RESA - height*slope\n" +
+                    "TORA = "+runwayParam.getTora()+" - "+distToTHR+" - "+ (stripEnd + RESA)+" - "+ slopeValue+ "\n" +
+                    "TORA = "+newTora+"\n \n" +
+                    "ASDA = "  + newAsda+ "\n \n" +
+                    "TODA = " + newToda+ " \n \n";
+        }
 
         // When displaying check if fisplacedThr == 0
+        /*
         if(displacedTHR == 0)
             calculationBrkdwn += "TORA = Distance from Threshold - Slope Calculation - Strip End \n" +
                     "TORA = " + distToTHR +" - "+slopeValue+" - "+stripEnd+"\n" +
@@ -103,9 +133,9 @@ public class Calculator {
         calculationBrkdwn += "TODA = Redeclared TORA\n"+"TODA = "+newTORA+"\n \n";
         calculationBrkdwn += "ASDA = Redeclared TORA\n"+"ASDA = "+newTORA+"\n \n";
         calculationBrkdwn += "LDA = Distance from Threshold - Strip End - RESA\n"+"LDA = "+distToTHR+" - "+stripEnd+" - "+RESA+"\n" + "LDA = " +newLDA+ "\n \n";
-
+        */
         //TODO add displaced threshold
-        runway.setRecalculatedParameters(new RunwayParameters(newTORA, newTORA, newTORA, newLDA, 0));
+        runway.setRecalculatedParameters(new RunwayParameters(newTora,newToda,newAsda,newLda, 0));
         runway.getRecalculatedParameters().setCalculationBrkdwn(calculationBrkdwn);
     }
 }
